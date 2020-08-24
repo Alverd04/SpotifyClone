a06119b98ceef55f39a919b1baf85f783892e0d5
Albert Font <albert.font.fernandez@gmail.com>
2020-08-24 20:32:34 +0200

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Nav = () =>{
+    return(
+        <div className='main'>
+            <div className='upperNav'>dummy text</div>
+            <div className='mainContent'>
+                <h1>Uniquely yours</h1>
+                <div className='card'>
+                    <div className='cardImage'>
+                        
+                    </div>
+                    <div className='cardContent'>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
